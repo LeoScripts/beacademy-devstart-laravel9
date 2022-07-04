fbd0c0abf5c317ac6841b1c7031f5ad1756a79f7
l <l@pop-os.localdomain>
2022-07-03 22:28:21 -0300

    feat: template

--- aula-02/mvc/resources/views/users/index.blade.php
+++ aula-02/mvc/resources/views/users/index.blade.php
@@ -1,26 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
-    <title>Document</title>
-</head>
-<body>
-    <!--
-        !!!!!! ATENÇÃO !!!!!!
-
-        se colocalocarmos o conteudo vindo do back com este documento em branco
-        desta forma
-        {{ $users }}
-
-        ele exibira os nossos dados em jason
-    -->
-
-
-    <div class="container">
+@extends('template.users')
+@section('title', 'Listagem de Usuarios')
+@section('body')
         <h1 class="bg-dark text-white p-3 mt-5 text-center">Listagem de Usuarios</h1>
 
         <table class="table">
@@ -46,6 +26,4 @@
                 @endForeach
             </tbody>
         </table>
-    </div>
-</body>
-</html>
+@endSection
--- aula-02/mvc/resources/views/users/show.blade.php
+++ aula-02/mvc/resources/views/users/show.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <title>Usuario: {{ $user->name }}</title>
-</head>
-<body>
-<div class="container">
+@extends('template.users')
+@section('title', 'Usuario')
+@section('body')
         <h1 class="bg-dark text-white p-3 mt-5 text-center">{{ $user->name }}</h1>
 
         <table class="table">
@@ -34,6 +26,4 @@
                 </tr>
             </tbody>
         </table>
-    </div>
-</body>
-</html>
+@endSection
