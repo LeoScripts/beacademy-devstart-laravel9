1a9d5e5e66f6259654ea7c29d21fb9404c08bc53
l <l@pop-os.localdomain>
2022-07-04 17:55:14 -0300

    feat: create user method two

--- aula-02/mvc/app/Http/Controllers/UserController.php
+++ aula-02/mvc/app/Http/Controllers/UserController.php
@@ -55,7 +55,7 @@ class UserController extends Controller
         $user = new User;
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = $request->password;
+        $user->password = bcrypt($request->password);
         $user->save();
 
         return redirect()->route('users.index');
